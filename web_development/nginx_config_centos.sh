11e7d18917ce409b4b6ed37f7ca27cc5b29a3536
Alex Ramsdell <foundling@users.noreply.github.com>
2015-02-10 15:45:30 -0500

    Update nginx_config_centos.sh

--- web_development/nginx_config_centos.sh
+++ web_development/nginx_config_centos.sh
@@ -1,30 +1,30 @@
-// as root, install nginx
+# as root, install nginx
 yum -y install nginx
 
-// create a location for your web root from which you willll serve data 
+#  create a location for your web root from which you willll serve data 
 mkdir /var/www
 chown -R nginx:nginx /var/www
 chmod 775 /var/www
 
-// start nginx
+#  start nginx
 nginx
 
-// to reload nginx
+#  to reload nginx
 nginx -s reload
 
-// to terminate nginx fatally
+#  to terminate nginx fatally
 nginx -s stop
 
-// to terminate nginx while letting current requests finish
+#  to terminate nginx while letting current requests finish
 nginx -s quit
 
-// to test the configuration files
+#  to test the configuration files
 nginx -t 
 
-// see the process and what it is using for a configuration file
+#  see the process and what it is using for a configuration file
 ps aux | grep -i nginx 
 
-// be aware that at the end of the default conf, the /etc/nginx/conf.d/*.conf files are usually included
-// means that they are loaded at the end of the main conf file
+#  be aware that at the end of the default conf, the /etc/nginx/conf.d/*.conf files are usually included
+#  means that they are loaded at the end of the main conf file
 ls /etc/nginx/nginx.conf
 
